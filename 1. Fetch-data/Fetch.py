ecdc1777282684066e63bfd7506fdf93b10e396a
Zhiming Zhang <zhiming41314@gmail.com>
2025-02-10 16:01:13 +0000

    trial1

--- 1. Fetch-data/Fetch.py
+++ 1. Fetch-data/Fetch.py
@@ -1,5 +1,7 @@
 import PyPDF2
-
+import fitz
+import os
+from PIL import Image
 def fetch_text(path: str) -> [str]:
     with open(path, 'rb') as pdf:
         reader = PyPDF2.PdfReader(pdf, strict=False)
@@ -9,11 +11,14 @@ def fetch_text(path: str) -> [str]:
             fetched_txt.append(txt)
     return fetched_txt
 
+def fetch_image(path: str):
+    file = fitz.open(path)
+    #Compute the number pages in the pdf
+    page_counts = len(file)
 
-
-
+    return 0
 if '__main__' == __name__:
-
-    txt = fetch_text('UnderstandingDeepLearning_11_21_24_C.pdf')
-    for word in txt:
-        print(word)
+    #txt = fetch_text('UnderstandingDeepLearning_11_21_24_C.pdf')
+    imgs = fetch_image('UnderstandingDeepLearning_11_21_24_C.pdf')
+    """for word in txt:
+        print(word)"""
