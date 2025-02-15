3c4631630b14d0913d7db29a31e7db35225b21dd
Zhiming Zhang <zhiming41314@gmail.com>
2025-02-15 13:23:23 +0000

    fetch_image function is added. It takes in a pdf path and fetch all images from the pdf into a given folder name

--- 1. Fetch-data/Fetch.py
+++ 1. Fetch-data/Fetch.py
@@ -2,6 +2,8 @@ import PyPDF2
 import fitz
 import os
 from PIL import Image
+
+
 def fetch_text(path: str) -> [str]:
     with open(path, 'rb') as pdf:
         reader = PyPDF2.PdfReader(pdf, strict=False)
@@ -11,14 +13,37 @@ def fetch_text(path: str) -> [str]:
             fetched_txt.append(txt)
     return fetched_txt
 
+
 def fetch_image(path: str):
     file = fitz.open(path)
-    #Compute the number pages in the pdf
+    # Compute the number pages in the pdf
     page_counts = len(file)
 
+    image_list = []
+    for page_num in range(page_counts):
+        content = file[page_num]
+        image_list.extend(content.get_images())
+    # [(reference number, , width, height, , color space)]
+    if len(image_list) == 0:
+        raise ValueError('No Image found')
+    for index, image in enumerate(image_list, start=1):
+        ref_num = image[0]
+        # Extract image
+        base_img = file.extract_image(ref_num)
+
+        img_byte = base_img['image']
+        # Store image extension/format
+        img_format = base_img['ext']
+        # Generate image file name
+        img_name = str(index) + '.' + img_format
+        with open(os.path.join('images/', img_name), 'wb') as image_file:
+            image_file.write(img_byte)
+            image_file.close()
     return 0
+
+
 if '__main__' == __name__:
-    #txt = fetch_text('UnderstandingDeepLearning_11_21_24_C.pdf')
-    imgs = fetch_image('UnderstandingDeepLearning_11_21_24_C.pdf')
-    """for word in txt:
-        print(word)"""
+    txt = fetch_text('UnderstandingDeepLearning_11_21_24_C.pdf')
+    fetch_image('UnderstandingDeepLearning_11_21_24_C.pdf')
+    for word in txt:
+        print(word)
