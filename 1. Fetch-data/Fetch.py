5330a0c634cd87098076e9f77e37d21d267bcbcd
Biangming4572398 <100448436+Biangming4572398@users.noreply.github.com>
2025-02-15 13:29:21 +0000

    Update Fetch.py
    Added more detail

--- 1. Fetch-data/Fetch.py
+++ 1. Fetch-data/Fetch.py
@@ -14,11 +14,10 @@ def fetch_text(path: str) -> [str]:
     return fetched_txt
 
 
-def fetch_image(path: str):
+def fetch_image(path: str, folder: str) -> None:
     file = fitz.open(path)
     # Compute the number pages in the pdf
     page_counts = len(file)
-
     image_list = []
     for page_num in range(page_counts):
         content = file[page_num]
@@ -36,7 +35,7 @@ def fetch_image(path: str):
         img_format = base_img['ext']
         # Generate image file name
         img_name = str(index) + '.' + img_format
-        with open(os.path.join('images/', img_name), 'wb') as image_file:
+        with open(os.path.join(f"{folder}/", img_name), 'wb') as image_file:
             image_file.write(img_byte)
             image_file.close()
     return 0
